787cdf19835c1650e80df91e5eecf1bf47b9d2a2
jasaiful <jasaiful@gmail.com>
2023-09-17 18:30:50 +0600

    added numbering in selected course list

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
-import Course from '../Course/Course';
 
 const Cart = ({ course, totalCredit, remaining, totalCoursePrice }) => {
-    
+
     return (
         <div className="md: w-1/4 mx-auto bg-white h-full rounded-xl p-2">
             <div>
@@ -11,16 +10,15 @@ const Cart = ({ course, totalCredit, remaining, totalCoursePrice }) => {
             </div>
             <div>
                 <h4 className="text-lg font-semibold my-5">Course Name</h4>
-                {
-                    course.map(course => <Course key={course.id} course={course}></Course>)
-                }
-                {/* <h4>Course: {course.length}</h4> */}
+                <ul className='font-semibold'>
+
+                    {course.map((course, index) => (<li key={course.id}>
+                        {index + 1}. {course.course_name} </li>))} </ul>
                 <br />
                 <hr />
                 <h4 className="font-semibold my-5">Total Credit Hour: {totalCredit} </h4>
             </div>
             <div>
-                
                 <hr />
             </div>
             <div>
@@ -35,7 +33,8 @@ Cart.propTypes = {
     course: PropTypes.array,
     totalCredit: PropTypes.func,
     remaining: PropTypes.func,
-    totalCoursePrice: PropTypes.func
+    totalCoursePrice: PropTypes.func,
+
 };
 
 export default Cart;
